d23c53665ef74a0aaf8a7f8e9626efea118485e0
codethazine <dario.finocchiaro01@gmail.com>
2023-08-04 00:56:43 +0200

    Reformat the script to work with Twitter V2

--- model/data_scripts/get_followed_accounts.py
+++ model/data_scripts/get_followed_accounts.py
@@ -1,26 +1,43 @@
-import tweepy
-import csv
-from dotenv import load_dotenv
 import os
+import json
+import csv
+import requests
+from dotenv import load_dotenv
 
-# Load the environment variables
+# Load environment variables
 load_dotenv()
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.getenv("TWITTER_API_KEY"), os.getenv("TWITTER_API_SECRET"))
-auth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_SECRET"))
+# Get the bearer token from environment variables
+bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
-# Create API object
-api = tweepy.API(auth)
+# Create URL with your user ID
+user_id = os.getenv("TWITTER_USER_ID")
+url = f"https://api.twitter.com/2/users/{user_id}/following"
 
-# Get the list of accounts you follow
-following = []
-for follow in tweepy.Cursor(api.friends).items():
-    following.append(follow.screen_name)
+print(user_id)
+
+# Set parameters
+params = {"user.fields": "username"}
+
+# Set headers
+headers = {"Authorization": f"Bearer {bearer_token}", "User-Agent": "v2FollowingLookupPython"}
+
+# Send request
+response = requests.request("GET", url, headers=headers, params=params)
+
+# Check response status
+if response.status_code != 200:
+    raise Exception("Request returned an error: {} {}".format(response.status_code, response.text))
+
+# Parse response
+json_response = response.json()
+
+# Extract usernames
+following = [user['username'] for user in json_response['data']]
 
 # Save the accounts to a CSV file
 with open('data/following.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['username'])
-    for follow in following:
-        writer.writerow([follow])
+    for user in following:
+        writer.writerow([user])
