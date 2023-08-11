4744b11834f24be16aeafd814e015244f5b1dd9c
codethazine <dario.finocchiaro01@gmail.com>
2023-08-11 22:10:33 +0200

    Add error handling on rate limit

--- model/data_scripts/get_last_100_tweets.py
+++ model/data_scripts/get_last_100_tweets.py
@@ -1,7 +1,7 @@
-import requests
 import os
-import json
 import csv
+import time
+import requests
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -12,6 +12,13 @@ bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
 if not bearer_token:
     raise ValueError("Please set your BEARER_TOKEN as an environment variable.")
 
+def handle_rate_limit(response):
+    if response.status_code == 429:
+        print("Rate limit exceeded. Waiting for 15 minutes + 1 second...")
+        time.sleep(15 * 60 + 1)  # 15 minutes + 1 second
+        return True
+    return False
+
 def get_user_id_from_username(username):
     url = f"https://api.twitter.com/2/users/by/username/{username}"
     headers = {
@@ -19,17 +26,20 @@ def get_user_id_from_username(username):
         "User-Agent": "v2UserLookupPython"
     }
     response = requests.get(url, headers=headers)
-    
+
+    while handle_rate_limit(response):
+        response = requests.get(url, headers=headers)
+
     if response.status_code != 200:
         raise Exception(f"Request returned an error: {response.status_code} {response.text}")
-        
+
     user_data = response.json()
     return user_data['data']['id']
 
 def get_last_100_tweets(user_id):
     # Create URL to fetch tweets
     url = f"https://api.twitter.com/2/users/{user_id}/tweets"
-    
+
     # Get parameters
     params = {
         "tweet.fields": "created_at,public_metrics,attachments",  # Added 'attachments' to check for media attachments
@@ -42,12 +52,14 @@ def get_last_100_tweets(user_id):
         "Authorization": f"Bearer {bearer_token}",
         "User-Agent": "v2UserTweetsPython"
     }
-
     response = requests.get(url, headers=headers, params=params)
-    
+
+    while handle_rate_limit(response):
+        response = requests.get(url, headers=headers, params=params)
+
     if response.status_code != 200:
         raise Exception(f"Request returned an error: {response.status_code} {response.text}")
-        
+
     return response.json()
 
 def main():
