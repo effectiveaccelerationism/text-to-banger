1b243ea8356002d4c6b149dd0552382011481370
Your Name <bagsintern@gmail.com>
2023-08-15 21:29:42 -0700

    terminal design w/ dark mode

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [generatedTweet, setGeneratedTweet] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [darkMode, setDarkMode] = useState(true);
+  const [contentType, setContentType] = useState('stocks');
+  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
   const handleTweetIdeaChange = (e) => {
     setTweetIdea(e.target.value);
@@ -16,7 +18,11 @@ function App() {
   const handleGenerateTweet = () => {
     setIsLoading(true);
     setGeneratedTweet(null);
-    axios.post('http://localhost:8080/generate-banger', {originalText: tweetIdea})
+    
+    axios.post(`${API_URL}/generate-banger`, {
+      originalText: tweetIdea,
+      contentType: contentType
+    })
     .then(response => {
       if (response.status !== 200) {
         throw new Error(`Request failed with status code ${response.status}`);
@@ -24,16 +30,13 @@ function App() {
       return response.data;
     })
     .then(data => {
-      if (data === 'Error generating banger tweet.') {
-        setGeneratedTweet("Error generating banger tweet.");
-      } else {
-        setGeneratedTweet(data);
-      }
-      setIsLoading(false);
+      setGeneratedTweet(data);
     })
     .catch(error => {
       console.error('An error occurred:', error);
       setGeneratedTweet("Error generating banger tweet.");
+    })
+    .finally(() => {
       setIsLoading(false);
     });
   };
@@ -44,12 +47,20 @@ function App() {
       document.documentElement.style.setProperty('--background-color', 'var(--light-background-color)');
       document.documentElement.style.setProperty('--text-color', 'var(--light-text-color)');
       document.documentElement.style.setProperty('--panel-background', 'var(--light-panel-background)');
+      document.documentElement.style.setProperty('--button-background', 'var(--light-button-background)');
+      document.documentElement.style.setProperty('--button-text', 'var(--light-button-text)');
+      document.documentElement.style.setProperty('--border-color', 'var(--light-border-color)');
+      document.documentElement.style.setProperty('--logo-text-color', 'var(--light-logo-text-color)');
     } else {
       document.documentElement.style.setProperty('--background-color', 'var(--dark-background-color)');
       document.documentElement.style.setProperty('--text-color', 'var(--dark-text-color)');
       document.documentElement.style.setProperty('--panel-background', 'var(--dark-panel-background)');
+      document.documentElement.style.setProperty('--button-background', 'var(--dark-button-background)');
+      document.documentElement.style.setProperty('--button-text', 'var(--dark-button-text)');
+      document.documentElement.style.setProperty('--border-color', 'var(--dark-border-color)');
+      document.documentElement.style.setProperty('--logo-text-color', 'var(--dark-logo-text-color)');
     }
-  };  
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -71,10 +82,10 @@ function App() {
       <header className="App-header">
         <div className="logo-container">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="text-logo">text-to-banger </h1>
+          <h1 className="text-logo">text-to-banger</h1>
         </div>
         <div className="content-container">
-          <form onSubmit={handleFormSubmit} className="tweet-form"> {/* Wrap everything in a form and attach submit handler */}
+          <form onSubmit={handleFormSubmit} className="tweet-form">
             <textarea
               id="tweetIdea"
               value={tweetIdea}
@@ -83,7 +94,12 @@ function App() {
               placeholder="What's happening?"
               rows="4"
             />
-            <button className="tweet-button" type="submit" disabled={isLoading}>Generate Banger Tweet</button> {/* set type to submit */}
+            {/* <select value={contentType} onChange={(e) => setContentType(e.target.value)}>
+              <option value="stocks">Stocks</option>
+              <option value="crypto">Crypto</option>
+              <option value="nft">NFTs</option>
+            </select> */}
+            <button className="tweet-button" type="submit" disabled={isLoading}>Generate Banger Tweet</button>
           </form>
           {isLoading && <p>generating a banger...</p>}
           <div className="generated-tweet-container">
@@ -110,3 +126,4 @@ function App() {
 }
 
 export default App;
+
