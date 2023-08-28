65668f30bae8ed69fc84e82bdd9d93b4872f2e6f
codethazine <dario.finocchiaro01@gmail.com>
2023-08-28 22:46:10 +0200

    Fix tweet cleaning

--- model/data_scripts/05_transform_data_for_finetuning.py
+++ model/data_scripts/05_transform_data_for_finetuning.py
@@ -1,22 +1,30 @@
 import re
 import json
 
-def clean_boring_tweet(tweet):
+import re
+
+def clean_tweet(tweet):
     # Remove hashtags
     tweet = re.sub(r'#\w+', '', tweet)
-
-    # Remove starting and ending single and double quotes, ending \" and \'
-    tweet = re.sub(r'^\'|^\"|\'$|\"$|\\\"$|\\\'$', '', tweet)
-
+    
     # Remove weird characters like \ud83c \udf1f \u2728
     tweet = re.sub(r'[\u2000-\u3300]|[\ud83c\udc00-\udfff\ud83d\ude00-\udfff]', '', tweet)
-
+    
+    # Remove all emojis
+    tweet = re.sub(r'[^\x00-\x7F]+', '', tweet)
+    
+    # Change \u2019 to '
+    tweet = re.sub(r'\u2019', '\'', tweet)
+    
     # Remove starting and ending whitespace
     tweet = tweet.strip()
-
+    
+    # Remove starting and ending single and double quotes
+    tweet = re.sub(r'^\'|^\"|\'$|\"$', '', tweet)
+    
     # Remove ending dots
     tweet = re.sub(r'\.$', '', tweet)
-
+    
     return tweet
 
 def transform_data(input_file, output_file):
@@ -30,12 +38,12 @@ def transform_data(input_file, output_file):
     for banger_tweet, boring_versions in data.items():
         for boring_version in boring_versions:
             # Clean the boring version
-            boring_version = clean_boring_tweet(boring_version)
+            boring_version = clean_tweet(boring_version)
 
             # Create a dict for each pair and append to transformed_data
             transformed_data.append({
                 "prompt": boring_version,
-                "completion": re.sub(r'\.$', '', banger_tweet) # Remove ending dots for data consistency
+                "completion": re.sub(r'\.$', '', clean_tweet(banger_tweet)) # Remove ending dots for data consistency
             })
 
     # Write the transformed data to the output file in the desired format
