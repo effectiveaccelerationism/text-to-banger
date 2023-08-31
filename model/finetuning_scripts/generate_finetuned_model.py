c853aff2f6fd5f1fa749817e01f644685b694dfa
codethazine <dario.finocchiaro01@gmail.com>
2023-08-31 20:28:55 +0200

    Add data formatting check script

--- model/finetuning_scripts/generate_finetuned_model.py
+++ model/finetuning_scripts/generate_finetuned_model.py
@@ -7,9 +7,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Create a file for the fine-tuning data
 openai.File.create(
-  file=open("data/final/bangers_finetuning_data.json", "rb"),
+  file=open("data/final/bangers_finetuning_data_prepared_chat.jsonl", "rb"),
   purpose='fine-tune'
 )
 
 # Create a fine-tuning job
-openai.FineTuningJob.create(training_file="text-to-banger-v2", model="gpt-3.5-turbo")
+openai.FineTuningJob.create(training_file="data/final/bangers_finetuning_data_prepared_chat.jsonl", model="gpt-3.5-turbo")
