3df82ce5fea150abd2e5186f3daab717eb003935
Your Name <bagsintern@gmail.com>
2023-09-06 14:20:24 -0700

    github + input

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,53 +1,49 @@
-// React Component (JSX)
-import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import QRCode from "qrcode.react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import "./App.css";
 
 function App() {
   const [tweetIdea, setTweetIdea] = useState("");
   const [generatedTweet, setGeneratedTweet] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [darkMode, setDarkMode] = useState(true); // Initially set to true
+  const [darkMode, setDarkMode] = useState(true);
   const [contentType, setContentType] = useState("stocks");
-  const [balance, setBalance] = useState(10);
-  const [showQRCode, setShowQRCode] = useState(false);
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
-  useEffect(() => {
+  const handleTweetIdeaChange = (e) => setTweetIdea(e.target.value);
+
+  const handleGenerateTweet = () => {
+    setIsLoading(true);
+    setGeneratedTweet(null);
+
+    axios
+      .post(`${API_URL}/generate-banger`, {
+        originalText: tweetIdea,
+        contentType: contentType,
+      })
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error(`Request failed with status code ${response.status}`);
+        }
+        return response.data;
+      })
+      .then((data) => {
+        setGeneratedTweet(data);
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+        setGeneratedTweet("Error generating banger tweet.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
     if (darkMode) {
-      document.documentElement.style.setProperty(
-        "--logo-text-color",
-        "var(--dark-logo-text-color)"
-      );
-      document.documentElement.style.setProperty(
-        "--background-color",
-        "var(--dark-background-color)"
-      );
-      document.documentElement.style.setProperty(
-        "--text-color",
-        "var(--dark-text-color)"
-      );
-      document.documentElement.style.setProperty(
-        "--panel-background",
-        "var(--dark-panel-background)"
-      );
-      document.documentElement.style.setProperty(
-        "--button-background",
-        "var(--dark-button-background)"
-      );
-      document.documentElement.style.setProperty(
-        "--button-text",
-        "var(--dark-button-text)"
-      );
-      document.documentElement.style.setProperty(
-        "--border-color",
-        "var(--dark-border-color)"
-      );
-    } else {
-      document.documentElement.style.setProperty(
-        "--logo-text-color",
-        "var(--light-logo-text-color)"
-      );
       document.documentElement.style.setProperty(
         "--background-color",
         "var(--light-background-color)"
@@ -72,37 +68,41 @@ function App() {
         "--border-color",
         "var(--light-border-color)"
       );
+      document.documentElement.style.setProperty(
+        "--logo-text-color",
+        "var(--light-logo-text-color)"
+      );
+    } else {
+      document.documentElement.style.setProperty(
+        "--background-color",
+        "var(--dark-background-color)"
+      );
+      document.documentElement.style.setProperty(
+        "--text-color",
+        "var(--dark-text-color)"
+      );
+      document.documentElement.style.setProperty(
+        "--panel-background",
+        "var(--dark-panel-background)"
+      );
+      document.documentElement.style.setProperty(
+        "--button-background",
+        "var(--dark-button-background)"
+      );
+      document.documentElement.style.setProperty(
+        "--button-text",
+        "var(--dark-button-text)"
+      );
+      document.documentElement.style.setProperty(
+        "--border-color",
+        "var(--dark-border-color)"
+      );
+      document.documentElement.style.setProperty(
+        "--logo-text-color",
+        "var(--dark-logo-text-color)"
+      );
     }
-  }, [darkMode]); // Watch for changes in darkMode state
-
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
-
-  const handleTweetIdeaChange = (e) => setTweetIdea(e.target.value);
-
-  const handleGenerateTweet = () => {
-    if (balance <= 0) {
-      setShowQRCode(true);
-      return;
-    }
-
-    setIsLoading(true);
-    setGeneratedTweet(null);
-
-    axios
-      .post(`${API_URL}/generate-banger`, {
-        originalText: tweetIdea,
-        contentType,
-      })
-      .then((response) => response.data)
-      .then((data) => {
-        setGeneratedTweet(data);
-        setBalance(balance - 1);
-      })
-      .catch(() => setGeneratedTweet("Error generating banger tweet."))
-      .finally(() => setIsLoading(false));
   };
-
-  const toggleDarkMode = () => setDarkMode(!darkMode);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -116,64 +116,76 @@ function App() {
     }
   };
 
-  const handlePaymentRedirect = () => {
-    window.location.href = "https://buy.stripe.com/00gaGE0mDd4o6K46oo";
-  };
-
-  const tweetGeneratedContent = () => {
-    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-      generatedTweet
-    )}`;
-    window.open(tweetUrl, "_blank");
-  };
-
   return (
     <div className="App">
-      <button className="mode-toggle" onClick={toggleDarkMode}>
-        {darkMode ? "🌙" : "☀️"}
-      </button>
+      <div className="icon-container">
+        <a
+          href="https://github.com/effectiveaccelerationism/text-to-banger"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="custom-button custom-icon">
+            <FontAwesomeIcon icon={faGithub} color="grey" />
+          </button>
+        </a>
 
+        <button className="mode-toggle" onClick={toggleDarkMode}>
+          {darkMode ? "🌙" : "☀️"}
+        </button>
+      </div>
       <header className="App-header">
-        <h1 className="text-logo">text-to-banger</h1>
+        <div className="logo-container">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="text-logo">text-to-banger</h1>
+        </div>
         <div className="content-container">
-          {showQRCode ? (
-            <>
-              <h2 className="action-title">Subscribe for unlimited tweets.</h2>
-              <div className="button-container">
-                <button
-                  className="share-button"
-                  onClick={handlePaymentRedirect}
+          <form onSubmit={handleFormSubmit} className="tweet-form">
+            <textarea
+              id="tweetIdea"
+              value={tweetIdea}
+              onChange={handleTweetIdeaChange}
+              onKeyDown={handleKeyDown}
+              placeholder="What's happening?"
+              rows="4"
+            />
+          </form>
+          <button
+            className="tweet-button generate-button"
+            onClick={handleGenerateTweet}
+            disabled={isLoading}
+          >
+            Generate Banger Tweet
+          </button>
+          {isLoading && <p>generating a banger...</p>}
+          <div className="generated-tweet-container">
+            {generatedTweet && (
+              <>
+                <p
+                  style={{
+                    color: generatedTweet.startsWith("Error")
+                      ? "darkred"
+                      : "inherit",
+                  }}
                 >
-                  Subscribe
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              {isLoading && <p>generating a banger...</p>}
-              <form onSubmit={handleFormSubmit} className="tweet-form">
-                {generatedTweet && <p>{generatedTweet}</p>}
-                <button
-                  className="tweet-button"
-                  type="submit"
-                  disabled={isLoading}
-                >
-                  {balance > 0 ? "Generate Banger Tweet" : "Subscribe"}
-                </button>
-                {generatedTweet && (
-                  <button
+                  {generatedTweet}
+                </p>
+                {!generatedTweet.startsWith("Error") && (
+                  <a
                     className="tweet-button"
-                    onClick={tweetGeneratedContent}
+                    href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                      generatedTweet
+                    )}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    Share Banger Tweet
-                  </button>
+                    Post Banger Tweet
+                  </a>
                 )}
-              </form>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
       </header>
-      <footer className="App-footer">{/* Footer content */}</footer>
     </div>
   );
 }
